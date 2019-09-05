078550f4062272d4637a28511a4c8a9c2bceec6a
raphaelkhan8 <raphaelkhan8@gmail.com>
2019-09-05 17:02:14 -0500

    serving static files using express... page is now loading

--- server/index.js
+++ server/index.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+app.use(express.static(path.join(__dirname, '../react-client/dist')));
 
 console.log('Hello World!');
 
