e7a2905b03d236200f89196587344c8bb0cdca86
Dan Murphy <dmurphy79@gmail.com>
2019-09-06 15:10:15 -0500

    Create App component in index.jsx file; no styling yet

---
+++ react-client/src/index.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {  }
+  }
+
+// Functions will go here
+
+  render() { 
+    return (  );
+  }
+}
+ 
+ReactDOM.render(<App />, document.getElementById('app'));
+
+export default App;
