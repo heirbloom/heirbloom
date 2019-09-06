e0d448932ccfb91d28b55e90074cced8b47dc0d5
Dan Murphy <dmurphy79@gmail.com>
2019-09-06 15:35:36 -0500

    import react-dom

--- react-client/src/index.jsx
+++ react-client/src/index.jsx
@@ -1,18 +1,24 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import NavBar from './components/NavBar.jsx';
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {  }
+    this.state = {};
   }
 
 // Functions will go here
 
   render() { 
-    return (  );
+    return (
+      <div>
+        <NavBar />
+      </div>
+    );
   }
 }
- 
+
 ReactDOM.render(<App />, document.getElementById('app'));
 
 export default App;
