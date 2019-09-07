5388f5dd48442221ea6016e12ff1607451cb952b
Dan Murphy <dmurphy79@gmail.com>
2019-09-07 14:30:46 -0500

    Adds classname to rendered div

--- react-client/src/App.jsx
+++ react-client/src/App.jsx
@@ -1,27 +1,25 @@
-import React, { Component } from 'react';
-import './App.css';
-import NavBar from './components/NavBar';
-import BackgroundImage from './components/BackgroundImage';
-import Login from './components/Login';
-
+import React, { Component } from "react";
+import "./App.css";
+import NavBar from "./components/NavBar";
+import BackgroundImage from "./components/BackgroundImage";
+import Login from "./components/Login";
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = { 
+    this.state = {
       ingredients: [],
-      view: 'login',
-    }
+      view: "login"
+    };
   }
-  render() { 
-    return (<div className="App">
-      <Login />
-      <NavBar />
-      <BackgroundImage />
-    </div>);
+  render() {
+    return (
+      <div className="App container-fluid m-0 p-0">
+        <NavBar />
+        <Login />
+      </div>
+    );
   }
 }
- 
+
 export default App;
-
-
