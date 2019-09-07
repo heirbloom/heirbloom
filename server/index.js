a4d59091afa4b30e833d4e6bfdc4c49dabd09ff1
raphaelkhan8 <raphaelkhan8@gmail.com>
2019-09-07 09:50:01 -0500

    serving signup/login routes

--- server/index.js
+++ server/index.js
@@ -1,33 +1,41 @@
+require('dotenv').config();
+
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
 const axios = require('axios');
-
-const app = express();
-app.use(bodyParser.json());
+const userRoutes = require('../routes/Users');
 
 const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// serve the signup/login routes
+app.use(userRoutes);
+// serve the static files
 app.use(express.static(path.join(__dirname, '../react-client/dist')));
 
 console.log('Hello World!');
 
-app.get('/',() => {
+app.get('/', () => {
 
-})
+});
 
 app.get('/', () => {
 
-})
+});
 
 app.post('/', () => {
 
-})
+});
 
 app.post('/', () => {
 
-})
+});
 
 app.listen(PORT, () => {
   console.log(`Hey im listening on port ${PORT}!`);
 });
-
