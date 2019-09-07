800958642202cf4b9e9a7bfe9298847e65f93639
Dan Murphy <dmurphy79@gmail.com>
2019-09-07 14:39:58 -0500

    Adds resizing for responsive login component

--- react-client/src/components/Login.jsx
+++ react-client/src/components/Login.jsx
@@ -14,9 +14,14 @@ import {
 
 const Login = () => {
   return (
-    <div class="bg">
+    <div class="bg pt-5">
       <Row className="mt-5">
-        <Col xs={{ size: 7, offset: 4 }} className="mt-5">
+        <Col
+          xl={{ size: 4, offset: 7 }}
+          md={{ size: 5, offset: 6 }}
+          xs={{ size: 10, offset: 1 }}
+          className="mt-5"
+        >
           <div className="login-head">
             <h3 className="ml-3">LOGIN</h3>
           </div>
