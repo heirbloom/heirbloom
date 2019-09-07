1afe72fecbe39fdf00d96d58b59d07fb6ff21c14
raphaelkhan8 <raphaelkhan8@gmail.com>
2019-09-07 18:00:01 -0500

    made an unfinished Profile component to test auth

---
+++ react-client/src/components/Profile.jsx
@@ -0,0 +1,21 @@
+// nothing important here for the time being... just a header... need it to test authentication
+
+import React from 'react';
+import '../App.css';
+import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
+
+
+const Profile = () => {
+  return (
+    <div className="container-fluid login-head position-relative">
+      <div className="row" >
+        <div className="col" >
+          <div className="login-head">
+            <h3>Profile Page</h3>
+          </div>
+        </div>
+      </div>
+    </div>);
+}
+
+export default Profile;
