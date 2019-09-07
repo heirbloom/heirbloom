952689f9bf1c913f4c8ddc61a5cab19791e44594
Dan Murphy <dmurphy79@gmail.com>
2019-09-07 10:44:47 -0500

    Add login Component, styling

--- react-client/src/components/Login.jsx
+++ react-client/src/components/Login.jsx
@@ -1,9 +1,25 @@
 import React, { Component } from 'react';
-
+import '../App.css';
 
 
 const Login = () => {
-  return (  );
+  return (
+    <div className="container-fluid login-head position-relative">
+      <div className="row" >
+        <div className="col" >
+          <div className="login-head">
+          <h3>LOGIN</h3>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col">
+          <div className="login-body">
+            
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>  );
 }
  
 export default Login;
