ba5ed1bf387409286cdff386e6872a8c91ad9e5b
raphaelkhan8 <raphaelkhan8@gmail.com>
2019-09-08 12:17:24 -0500

    signup is working... saves user info in db and redirects to landing page

--- react-client/src/components/Signup.jsx
+++ react-client/src/components/Signup.jsx
@@ -14,37 +14,46 @@ import {
 } from 'reactstrap';
 import { baseUrl } from '../constants';
 
+
 class Signup extends React.Component {
-  state = {
-    userCredentials: {
-      username: '',
-      email: '',
-      password: '',
-      zipcode: '',
+  constructor(props) {
+    super(props);
+    this.state = {
+      userCredentials: {
+        username: '',
+        email: '',
+        password: '',
+        zipcode: '',
+      }
     }
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
   }
-  handleSubmit = (event) => {
+
+  handleSubmit(event) {
     event.preventDefault();
     const { username, email, password, zipcode } = this.state.userCredentials;
-    // ifany field is blank, alert the user
+    // if any field is blank, alert the user
     if (!username || !email || !password || !zipcode) {
       return alert('All fields are required!')
     }
-    // else, send a post request to the login route to allow server to save login info to the database
+    // else, send a post request to the server to save login info in the database
     axios.post(`${baseUrl}/api/signup`, this.state.userCredentials)
       .then((response) => {
-        // if user successfully signs up, redirect them to login page
-        this.props.history.push('/login');
-        // alert the user that they've signed up and ask them to login
-        return alert('You have successfully signed-up! Please login.');
+        // console.log('RESPONSE', response.data)
+        // reposnse is an object with a data property which is an object containing the signup info
+        // if user successfully signs up, give them a user token and redirect to the landing page
+        sessionStorage.setItem('token', response.data.token);
+        this.props.history.push('/landing', { isLoggedIn: true });
+        return alert('Thank you for signing up. Welcome to Heirbloom!');
       })
       .catch((err) => {
         console.error(err);
-        alert('Problem signing-up, please check your sign-up information and try again!');
+        alert('The username and/or e-mail are already taken. Please try again.');
       })
   }
 
-  handleChange = (event) => {
+  handleChange(event) {
     // take the name and input value for each from the form
     const { value, name } = event.target;
     const userCredentials = this.state.userCredentials;
@@ -53,9 +62,10 @@ class Signup extends React.Component {
     // set the state to those input values
     this.setState({ userCredentials });
   }
+  
   render() {
     return (
-      <div class="bg pt-5">
+      <div className="bg pt-5">
         <Row className="mt-5">
           <Col
             xl={{ size: 4, offset: 7 }}
