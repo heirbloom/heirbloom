fee8c8080d26882f265286bfbb36fdabfbfc6c1e
raphaelkhan8 <raphaelkhan8@gmail.com>
2019-09-07 22:56:58 -0500

    readjusted header to match login style

--- react-client/src/components/Profile.jsx
+++ react-client/src/components/Profile.jsx
@@ -2,20 +2,26 @@
 
 import React from 'react';
 import '../App.css';
-import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
+import { Button, Form, FormGroup, Label, Input, FormText, Row, Col } from 'reactstrap';
 
 
 const Profile = () => {
   return (
-    <div className="container-fluid login-head position-relative">
-      <div className="row" >
-        <div className="col" >
-          <div className="login-head">
-            <h3>Profile Page</h3>
-          </div>
+  <div class="bg pt-5">
+    <Row className="mt-5">
+      <Col
+        xl={{ size: 4, offset: 7 }}
+        md={{ size: 5, offset: 6 }}
+        xs={{ size: 10, offset: 1 }}
+        className="mt-5"
+      >
+        <div className="login-head">
+          <h3 className="ml-3">PROFILE PAGE</h3>
         </div>
-      </div>
-    </div>);
+      </Col>
+    </Row>
+  </div>
+  );
 }
 
 export default Profile;
