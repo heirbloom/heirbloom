f6ebe7f8d377cde982fadbc2b22b2cb76519e676
sdelafuente12 <s.delafuente831@gmail.com>
2019-09-08 19:47:44 -0500

    commented out some functions

--- server/index.js
+++ server/index.js
@@ -1,34 +1,32 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
-const axios = require('axios');
+// const axios = require('axios');
 
 const app = express();
 app.use(bodyParser.json());
 
-app.use(express.static(path.join(__dirname, '../react-client/dist')));
+app.use(express.static(path.join(__dirname, '/../react-client/public')));
+app.use(express.static(path.join(__dirname, '/../react-client/dist')));
 
 const PORT = process.env.PORT || 3000;
 
-console.log('Hello World!');
+// app.get('/zipcode',() => {
 
-app.get('/',() => {
+// })
 
-})
+// app.get('/', () => {
 
-app.get('/', () => {
+// })
 
-})
+// app.post('/', () => {
 
-app.post('/', () => {
+// })
 
-})
+// app.post('/', () => {
 
-app.post('/', () => {
-
-})
+// })
 
 app.listen(PORT, () => {
   console.log(`Hey im listening on port ${PORT}!`);
 });
-
