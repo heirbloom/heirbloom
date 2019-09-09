ba33c190229ad6ec8ce8a8d4d1223186e64ded7d
raphaelkhan8 <raphaelkhan8@gmail.com>
2019-09-09 12:34:38 -0500

    removed cors

--- server/index.js
+++ server/index.js
@@ -3,7 +3,6 @@ require('dotenv').config();
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
-const cors = require('cors');
 const userRoutes = require('../routes/Users');
 
 const { getMarketsInfo } = require('./apiHelpers');
@@ -11,8 +10,6 @@ const { getMarketsInfo } = require('./apiHelpers');
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-// use cors to allow server and client to run on different ports
-app.use(cors());
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, '/../react-client/public')));
@@ -20,10 +17,8 @@ app.use(express.static(path.join(__dirname, '/../react-client/dist')));
 
 // serve the signup/login routes
 app.use('/api', userRoutes);
-// serve the static index.html file in React-client folder
-app.use(express.static(path.join(__dirname, '../react-client/public')));
 
-app.post('/usdaResponse', (req, res) => {
+app.post('/api/usdaResponse', (req, res) => {
   getMarketsInfo(70118)
     .then((marketInfo) => {
       res.send(marketInfo.map((marketObj) => marketObj.data));
