bd208b27df9ec1bf0942195b3cbbf24199389ed0
sdelafuente12 <s.delafuente831@gmail.com>
2019-09-09 10:58:36 -0500

    syntax error

--- server/apiHelpers.js
+++ server/apiHelpers.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 
 const getMarketsInfo = (zip) => {
   return axios.get(`http://search.ams.usda.gov/farmersmarkets/v1/data.svc/zipSearch?zip=${zip}`)
-    .then((res) => 8res.data.results.map((market) => axios.get(`http://search.ams.usda.gov/farmersmarkets/v1/data.svc/mktDetail?id=${market.id}`)))
+    .then((res) => res.data.results.map((market) => axios.get(`http://search.ams.usda.gov/farmersmarkets/v1/data.svc/mktDetail?id=${market.id}`)))
     .then((promArray) => axios.all(promArray))
     .catch((err) => console.log(err));
 };
