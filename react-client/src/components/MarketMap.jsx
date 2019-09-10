e6c7426509f3a6f0b567cd1c3265e9743bf1d6b0
Dan Murphy <dmurphy79@gmail.com>
2019-09-10 09:17:07 -0500

    Adds MarketMap component

--- react-client/src/components/MarketMap.jsx
+++ react-client/src/components/MarketMap.jsx
@@ -4,7 +4,7 @@ import { Col, Row, Container } from "reactstrap";
 const MarketMap = () => {
   return (
     <Col id="market-map" className="mt-10" sm={{ size: 9, offset: 3 }}>
-      <h1>Tom Tom Map gonna go here</h1>
+      <h1>Tom Tom Map is gonna go here</h1>
     </Col>
   );
 };
