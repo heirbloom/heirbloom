28ffc6119f248ed1a1342433b132dcc23ba445f4
raphaelkhan8 <raphaelkhan8@gmail.com>
2019-09-10 19:23:14 -0500

    component made to set up routing... only has a header rn

---
+++ react-client/src/components/Profile.jsx
@@ -0,0 +1,33 @@
+// not much here for the time being... just a header... need it to set up routes
+
+import React, { Fragment } from 'react';
+import NavBar from "./NavBar.jsx";
+import '../App.css';
+import { Button, Form, FormGroup, Label, Input, FormText, Row, Col } from 'reactstrap';
+
+const Profile = (props) => {
+  const { username, email, zipcode } = props.user;
+  console.log('PROFILE', [username, email, zipcode]);
+
+  return (
+    <Fragment>
+      <NavBar user={props.user} />
+      <div className="bg pt-5">
+        <Row className="mt-5">
+          <Col
+            xl={{ size: 4, offset: 7 }}
+            md={{ size: 5, offset: 6 }}
+            xs={{ size: 10, offset: 1 }}
+            className="mt-5"
+          >
+            <div className="login-head">
+              <h3 className="ml-3">{`${username}'s`} Profile Page</h3>
+            </div>
+          </Col>
+        </Row>
+      </div>
+    </Fragment>
+  );
+}
+
+export default Profile;
