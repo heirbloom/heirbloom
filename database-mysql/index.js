ce42704df509b3509e989fda94b49692a2427ce5
sdelafuente12 <s.delafuente831@gmail.com>
2019-09-11 13:24:55 -0500

    added db

--- database-mysql/index.js
+++ database-mysql/index.js
@@ -5,8 +5,9 @@ const Sequelize = require('sequelize');
 const sequelize = new Sequelize({
   host: process.env.HOST,
   username: process.env.USER_NAME,
-  port: process.env.PORT,
+  port: process.env.DB_PORT,
   password: process.env.USER_PASSWORD,
+  database: process.env.DATABASE,
   dialect: 'mysql',
 });
 
