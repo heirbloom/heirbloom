9d8af8dbbf0b27b088c270fe7022cb2e84d137e0
raphaelkhan8 <raphaelkhan8@gmail.com>
2019-09-11 15:23:27 -0500

    added user's zip code... still needs to be made pretty

--- react-client/src/components/MarketList.jsx
+++ react-client/src/components/MarketList.jsx
@@ -14,6 +14,7 @@ const MarketList = (props) => {
       <Container>
         <Row className="mt-10 mb-2 w-100">
           <h1 className="headline ml-3">Find a market.</h1>
+          <h2>Current Zip Code: {`${zipcode}`}</h2>
         </Row>
         <Row>
           <MarketMap />
