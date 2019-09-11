81528741384bc50bac7c80f35d735f8332bab05e
raphaelkhan8 <raphaelkhan8@gmail.com>
2019-09-11 15:25:28 -0500

    pasted usdaResponse api route back in

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const path = require('path');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const userRoutes = require('../routes/Users');
-// const models = require('../database-mysql');
+const models = require('../database-mysql');
 
 const {
   getMarketsInfo,
@@ -17,14 +17,39 @@ app.use(bodyParser.urlencoded({
   extended: true,
 }));
 app.use(bodyParser.json());
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
+
 
 // serve the signup/login routes
 app.use('/api', userRoutes);
 
 app.post('/api/usdaResponse', (req, res) => {
+  // const { email } = req.body;
+  // req.body is a JSON object nested in a regular object
+  const keys = Object.keys(req.body);
+  // console.log('KEYSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS', keys);
+  const { email } = JSON.parse(keys[0]);
+  // query the database for the user with the input email
+  models.Users.findOne({ where: { email } })
+    .then((foundUser) => {
+      if (!foundUser) {
+        return res.status(404).json('User not found');
+      }
+      // if the user exists:
+      // console.log(foundUser);
+      // foundUser is an object with the user info from the db; pass the zipcode to getMarketsInfo
+      getMarketsInfo(foundUser.zipcode)
+        .then((marketInfo) => {
+          console.log(marketInfo);
+          res.send(marketInfo.map((marketObj) => marketObj.data));
+        })
+        .catch((err) => console.log(err));
+    });
+});
+
+app.post('/api/usdaResponse', (req, res) => {
   console.log(req.body);
-  getMarketsInfo(70118);
+  getMarketsInfo(req.body.zipcode);
 });
 
 // app.get('/', () => {
@@ -42,7 +67,6 @@ app.post('/api/usdaResponse', (req, res) => {
 app.use(express.static(path.join(__dirname, '/../react-client/public')));
 app.use(express.static(path.join(__dirname, '/../react-client/dist')));
 
-// serve index.html
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '/../react-client/dist/index.html'));
 });
