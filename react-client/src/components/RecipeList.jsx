474ca62e97120e1fa965c781efe5bb6ac1052094
raphaelkhan8 <raphaelkhan8@gmail.com>
2019-09-11 21:09:29 -0500

    made RecipeList the landing page/added NavBar

--- react-client/src/components/RecipeList.jsx
+++ react-client/src/components/RecipeList.jsx
@@ -1,6 +1,7 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import RecipeListItem from "./RecipeListItem.jsx";
 import { Col, Row, Container } from "reactstrap";
+import NavBar from "./NavBar.jsx";
 
 class RecipeList extends Component {
   constructor(props) {
@@ -8,29 +9,36 @@ class RecipeList extends Component {
     this.state = {};
   }
   render() {
+    console.log('recipes!!!!!!!!!!!', this.props);
+    const { user } = this.props;
+    // const { username, email, zipcode } = this.props.user;
+    // console.log('LANDING', [username, email, zipcode]);
     return (
-      <Container>
-        <Row>
-          <Col>
-            <h1 className="headline mt-10 mb-2">
-              Buy it fresh. Make it fresh.
-            </h1>
-          </Col>
-        </Row>
-        <Row>
-          <Col md={{ size: 8 }}>
-            <RecipeListItem />
-            <RecipeListItem />
-            <RecipeListItem />
-          </Col>
-          <Col md={{ size: 3, offset: 1 }}>
-            test text Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-            Architecto quo itaque eveniet dolorum nisi repellendus, quia sit
-            nostrum, distinctio delectus dolorem nulla, deleniti labore illum
-            doloremque numquam harum magni tempore?
-          </Col>
-        </Row>
-      </Container>
+      <Fragment>
+        <NavBar user={user} />
+        <Container>
+          <Row>
+            <Col>
+              <h1 className="headline mt-10 mb-2">
+                Buy it fresh. Make it fresh.
+              </h1>
+            </Col>
+          </Row>
+          <Row>
+            <Col md={{ size: 8 }}>
+              <RecipeListItem />
+              <RecipeListItem />
+              <RecipeListItem />
+            </Col>
+            <Col md={{ size: 3, offset: 1 }}>
+              test text Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+              Architecto quo itaque eveniet dolorum nisi repellendus, quia sit
+              nostrum, distinctio delectus dolorem nulla, deleniti labore illum
+              doloremque numquam harum magni tempore?
+            </Col>
+          </Row>
+        </Container>
+      </Fragment>
     );
   }
 }
