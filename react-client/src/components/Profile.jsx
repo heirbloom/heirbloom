765578ff2337f2ab82abaa13a452065915f8a27c
raphaelkhan8 <raphaelkhan8@gmail.com>
2019-09-11 20:02:26 -0500

    took out api request

--- react-client/src/components/Profile.jsx
+++ react-client/src/components/Profile.jsx
@@ -21,7 +21,7 @@ const Profile = (props) => {
             className="mt-5"
           >
             <div className="login-head">
-              <h3 className="ml-3">{`${username}'s`} Profile Page</h3>
+              <h3 className="ml-3">{`${username}`.toUpperCase() + "'s"} Profile Page</h3>
             </div>
           </Col>
         </Row>
