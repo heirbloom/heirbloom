e3cdb9152f613364dfacb5033d45f995ae421337
Dan Murphy <dmurphy79@gmail.com>
2019-09-12 14:56:33 -0500

    Adds ZipCodeModal to MarketList Component

--- react-client/src/components/MarketList.jsx
+++ react-client/src/components/MarketList.jsx
@@ -2,9 +2,10 @@ import React, { Fragment } from "react";
 import MarketListItem from "./MarketListItem.jsx";
 import { Container, Row, Col } from "reactstrap";
 import MarketMap from "./MarketMap.jsx";
-import NavBar from './NavBar.jsx';
+import NavBar from "./NavBar.jsx";
+import ZipcodeModal from "./ZipcodeModal.jsx";
 
-const MarketList = (props) => {
+const MarketList = props => {
   // console.log('MarketList props', props.localMarkets);
   const { localMarkets, user } = props;
   const { zipcode } = props.user;
@@ -15,12 +16,16 @@ const MarketList = (props) => {
       <Container>
         <Row className="mt-10 mb-2 w-100">
           <h1 className="headline ml-3">Find a market.</h1>
-          <h2>Current Zip Code: {`${zipcode}`}</h2>
         </Row>
         <Row>
           <MarketMap />
-          <Col md={{ size: 4, offest: 0 }}>
-            {localMarkets.map((marketInfo, index) => <MarketListItem marketInfo={marketInfo} key={index} />)}
+          <Col>
+            <div className="ml-3 mr-3">Home Zip Code: {`${zipcode}`}</div>
+            <ZipcodeModal zipcode={zipcode} />
+            <hr />
+            {localMarkets.map((marketInfo, index) => (
+              <MarketListItem marketInfo={marketInfo} key={index} />
+            ))}
           </Col>
         </Row>
       </Container>
