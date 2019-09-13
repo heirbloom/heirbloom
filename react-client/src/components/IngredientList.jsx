f7510c64809041f2d31e3bbebb6ac49ebf678469
Dan Murphy <dmurphy79@gmail.com>
2019-09-12 20:21:32 -0500

    Ties Ingredients and IngredientList into database

--- react-client/src/components/IngredientList.jsx
+++ react-client/src/components/IngredientList.jsx
@@ -1,28 +1,28 @@
 import React, { Fragment } from "react";
 import Ingredient from "./Ingredient.jsx";
 import { Container, Row, Col } from "reactstrap";
-import NavBar from './NavBar.jsx';
+import NavBar from "./NavBar.jsx";
 
 const IngredientList = props => {
-  const { user } = props;
+  const { user, ingredients } = props;
   return (
     <Fragment>
       <NavBar user={user} />
-    <Container fluid>
-      <Row className="mt-10 ml-1">
-        <Col xs={{ size: 9 }}>
-          <h1 className="headline">
-            Here&rsquo;s what&rsquo;s fresh in New Orleans.
-          </h1>
-          <p>
-            Not in New Orleans? Click <a>Here.</a>
-          </p>
-        </Col>
-      </Row>
-      <Row className="ml-1">
-        <Ingredient />
-      </Row>
-    </Container>
+      <Container fluid>
+        <Row className="mt-10 ml-1">
+          <Col xs={{ size: 9 }}>
+            <h1 className="headline">
+              Here&rsquo;s what&rsquo;s fresh in New Orleans.
+            </h1>
+            <p>
+              Not in New Orleans? Click <a>Here.</a>
+            </p>
+          </Col>
+        </Row>
+        <Row className="ml-1">
+          <Ingredient ingredients={ingredients} />
+        </Row>
+      </Container>
     </Fragment>
   );
 };
