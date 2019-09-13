a1625356013a8c9cda638df26d14f44444146414
Dan Murphy <dmurphy79@gmail.com>
2019-09-13 12:08:28 -0500

    Adds style to FavRecipes

--- react-client/src/components/FavRecipes.jsx
+++ react-client/src/components/FavRecipes.jsx
@@ -21,64 +21,66 @@ const FavRecipes = props => {
 
   return (
     <Fragment>
-      <NavBar user={props.user} />
-      <Container>
-        <Row className="mt-10 position-relative">
-          <Col>
-            <h5>{`${username}'s favorite recipes`}</h5>
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            <Table>
-              <tbody>
-                <tr>
-                  <td>Strawberry Shortcake</td>
-                  <td>
-                    <Button size="sm" color="success">
-                      Click to view
-                    </Button>
-                  </td>
-                  <td>
-                    <Button
-                      color="white"
-                      className="far fa-heart float-right"
-                    ></Button>
-                  </td>
-                </tr>
-                <tr>
-                  <td>Strawberry Ice Cream</td>
-                  <td>
-                    <Button size="sm" color="success">
-                      Click to view
-                    </Button>
-                  </td>{" "}
-                  <td>
-                    <Button
-                      color="white"
-                      className="far fa-heart float-right"
-                    ></Button>
-                  </td>
-                </tr>
-                <tr>
-                  <td>Strawberry Rhubarb Pie</td>
-                  <td>
-                    <Button size="sm" color="success">
-                      Click to view
-                    </Button>
-                  </td>
-                  <td>
-                    <Button
-                      color="white"
-                      className="far fa-heart float-right"
-                    ></Button>
-                  </td>
-                </tr>
-              </tbody>
-            </Table>
-          </Col>
-        </Row>
-      </Container>
+      <div className="bg-recipe pt-5">
+        <NavBar user={props.user} className="mb-3" />
+        <Container md={{ size: 6, offset: 3 }}>
+          <Row className="mt-5 position-relative bg-white recipe-table">
+            <Col>
+              <h5 className="text-capitalize pt-3">{`${username}'s favorite recipes`}</h5>
+            </Col>
+          </Row>
+          <Row className="bg-white recipe-table">
+            <Col>
+              <Table hover>
+                <tbody>
+                  <tr>
+                    <td>Strawberry Shortcake</td>
+                    <td>
+                      <Button className="float-right" size="sm" color="warning">
+                        Click to view
+                      </Button>
+                    </td>
+                    <td>
+                      <Button
+                        color="white"
+                        className="fas fa-heart float-right text-danger"
+                      ></Button>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>Strawberry Shortcake</td>
+                    <td>
+                      <Button className="float-right" size="sm" color="warning">
+                        Click to view
+                      </Button>
+                    </td>
+                    <td>
+                      <Button
+                        color="white"
+                        className="fas fa-heart float-right text-danger"
+                      ></Button>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>Strawberry Shortcake</td>
+                    <td>
+                      <Button className="float-right" size="sm" color="warning">
+                        Click to view
+                      </Button>
+                    </td>
+                    <td>
+                      <Button
+                        color="white"
+                        className="fas fa-heart float-right text-danger"
+                      ></Button>
+                    </td>
+                  </tr>
+                </tbody>
+              </Table>
+            </Col>
+          </Row>
+        </Container>
+      </div>
     </Fragment>
   );
 };
