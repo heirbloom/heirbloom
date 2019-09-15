b2f663b6b64ccfa0ea2faff087eb3b63df1ecb72
Dan Murphy <dmurphy79@gmail.com>
2019-09-15 16:00:28 -0500

    Adds styles to popup alerts

--- react-client/src/components/Signup.jsx
+++ react-client/src/components/Signup.jsx
@@ -1,7 +1,8 @@
 import React, { Fragment } from "react";
-import NavBar from './NavBar.jsx';
+import NavBar from "./NavBar.jsx";
 import axios from "axios";
 import "../App.css";
+import Swal from "sweetalert2";
 import {
   Button,
   Form,
@@ -33,13 +34,19 @@ class Signup extends React.Component {
 
   handleSubmit(event) {
     event.preventDefault();
-    const { username, email, password, zipcode, confirmPassword } = this.state.userCredentials;
+    const {
+      username,
+      email,
+      password,
+      zipcode,
+      confirmPassword
+    } = this.state.userCredentials;
     // if any field is blank, alert the user
     if (!username || !email || !password || !zipcode || !confirmPassword) {
-      return alert("All fields are required.");
+      return Swal.fire("All fields are required.");
     }
     if (password !== confirmPassword) {
-      return alert("Passwords do not match.");
+      return Swal.fire("Passwords do not match.");
     }
     // else, send a post request to the server to save login info in the database
     axios
@@ -50,11 +57,11 @@ class Signup extends React.Component {
         // if user successfully signs up, give them a user token and redirect to the landing page
         sessionStorage.setItem("token", response.data.token);
         this.props.history.push("/ingredient-list", { isLoggedIn: true });
-        return alert("Thank you for signing up. Welcome to Heirbloom!");
+        return Swal.fire("Thank you for signing up. Welcome to Heirbloom!");
       })
       .catch(err => {
         console.error(err);
-        alert(
+        Swal.fire(
           "The username and/or e-mail are already taken. Please try again."
         );
       });
@@ -74,88 +81,88 @@ class Signup extends React.Component {
     return (
       <Fragment>
         <NavBar />
-      <div className="signup-bg pt-5">
-        <Row className="mt-5">
-          <Col
-            xl={{ size: 4, offset: 7 }}
-            md={{ size: 5, offset: 6 }}
-            xs={{ size: 10, offset: 1 }}
-            className="mt-5"
-          >
-            <div className="login-head">
-              <h3 className="ml-3">SIGN-UP</h3>
-            </div>
-            <div className="login-body pt-3">
-              <div className="login-form">
-                <Form onSubmit={this.handleSubmit}>
-                  <FormGroup>
-                    <Input
-                      onChange={this.handleChange}
-                      type="text"
-                      name="username"
-                      id="exampleUsername"
-                      placeholder="username"
-                      className="ml-3 col-11"
-                    />
-                  </FormGroup>
-                  <FormGroup>
-                    <Input
-                      onChange={this.handleChange}
-                      type="text"
-                      name="zipcode"
-                      id="exampleZipcode"
-                      placeholder="zipcode"
-                      className="ml-3 col-11"
-                    />
-                  </FormGroup>
-                  <FormGroup>
-                    <Input
-                      onChange={this.handleChange}
-                      type="email"
-                      name="email"
-                      id="exampleEmail"
-                      placeholder="email"
-                      className="ml-3 col-11"
-                    />
-                  </FormGroup>
-                  <FormGroup>
-                    <Input
-                      onChange={this.handleChange}
-                      type="password"
-                      name="password"
-                      id="password"
-                      placeholder="password"
-                      className="ml-3 col-11"
-                    />
-                  </FormGroup>
-                  <FormGroup>
-                    <Input
-                      onChange={this.handleChange}
-                      type="password"
-                      name="confirmPassword"
-                      id="confirmPassword"
-                      placeholder="confirm password"
-                      className="ml-3 col-11"
-                    />
-                  </FormGroup>
-                  <Row>
-                    <Col className="col-12">
-                      <Button
-                        type="submit"
-                        id="login-button"
-                        className="float-right mr-4 mb-3"
-                      >
-                        sign me up!
-                      </Button>
-                    </Col>
-                  </Row>
-                  {/* <button type="submit">Login</button> */}
-                </Form>
+        <div className="signup-bg pt-5">
+          <Row className="mt-5">
+            <Col
+              xl={{ size: 4, offset: 7 }}
+              md={{ size: 5, offset: 6 }}
+              xs={{ size: 10, offset: 1 }}
+              className="mt-5"
+            >
+              <div className="login-head">
+                <h3 className="ml-3">SIGN-UP</h3>
               </div>
-            </div>
-          </Col>
-        </Row>
-      </div>
+              <div className="login-body pt-3">
+                <div className="login-form">
+                  <Form onSubmit={this.handleSubmit}>
+                    <FormGroup>
+                      <Input
+                        onChange={this.handleChange}
+                        type="text"
+                        name="username"
+                        id="exampleUsername"
+                        placeholder="username"
+                        className="ml-3 col-11"
+                      />
+                    </FormGroup>
+                    <FormGroup>
+                      <Input
+                        onChange={this.handleChange}
+                        type="text"
+                        name="zipcode"
+                        id="exampleZipcode"
+                        placeholder="zipcode"
+                        className="ml-3 col-11"
+                      />
+                    </FormGroup>
+                    <FormGroup>
+                      <Input
+                        onChange={this.handleChange}
+                        type="email"
+                        name="email"
+                        id="exampleEmail"
+                        placeholder="email"
+                        className="ml-3 col-11"
+                      />
+                    </FormGroup>
+                    <FormGroup>
+                      <Input
+                        onChange={this.handleChange}
+                        type="password"
+                        name="password"
+                        id="password"
+                        placeholder="password"
+                        className="ml-3 col-11"
+                      />
+                    </FormGroup>
+                    <FormGroup>
+                      <Input
+                        onChange={this.handleChange}
+                        type="password"
+                        name="confirmPassword"
+                        id="confirmPassword"
+                        placeholder="confirm password"
+                        className="ml-3 col-11"
+                      />
+                    </FormGroup>
+                    <Row>
+                      <Col className="col-12">
+                        <Button
+                          type="submit"
+                          id="login-button"
+                          className="float-right mr-4 mb-3"
+                        >
+                          sign me up!
+                        </Button>
+                      </Col>
+                    </Row>
+                    {/* <button type="submit">Login</button> */}
+                  </Form>
+                </div>
+              </div>
+            </Col>
+          </Row>
+        </div>
       </Fragment>
     );
   }
