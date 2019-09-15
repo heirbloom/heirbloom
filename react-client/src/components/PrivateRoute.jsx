217323e3392137f5f6800464672ea15928a1febc
raphaelkhan8 <raphaelkhan8@gmail.com>
2019-09-14 22:54:00 -0500

    figured out how to pass down functions with React Context so passing functions here and then to other components

--- react-client/src/components/PrivateRoute.jsx
+++ react-client/src/components/PrivateRoute.jsx
@@ -2,20 +2,17 @@ import React from "react";
 import { Route, Redirect } from "react-router-dom";
 
 const PrivateRoute = props => {
-  // extract the properties passed down from App component
+
+  // destructure the properties passed down from App component
   const {
-    path,
-    isAuthenticated,
-    component: Component,
-    setAuth,
-    user,
-    recipes,
-    ingredients,
-    userLocation,
-    localMarkets,
-    marketCoordinates,
-    sessionZipcode
+    // these are needed for auth; setAuth is the setAuthentication function
+    path, isAuthenticated, component: Component, setAuth,
+    // these are the non-auth functions being passed down
+    getFavRecipes, handleRecipes, addToFavorites,
+    // state props being passed down
+    user, recipes, ingredients, userLocation, localMarkets, favRecipes, sessionZipcode
   } = props;
+
   return (
     // render the private route dynamically
     <Route
@@ -23,7 +20,7 @@ const PrivateRoute = props => {
       path={path}
       render={routeProps => {
         const { isLoggedIn } = routeProps.location.state || {};
-        // if user is logged in, has a token but isn't authenticated
+        // if user is logged in and has a token but isn't authenticated
         if (
           isLoggedIn &&
           !!sessionStorage.getItem("token") &&
@@ -33,15 +30,20 @@ const PrivateRoute = props => {
           setAuth(isLoggedIn);
         }
         // else, if they are authenticated, pass down the user info to whatever component they visit
-        // otherwise (menaing they aren't authenticated), redirect them to home
+        // otherwise (meaning they aren't authenticated), redirect them to home
+        // NOTE: If you wanna pass down a prop to a PrivateRoute component, pass it here first!!!
+        //       (also destructure it above to keep things uniform)
         return isAuthenticated ? (
           <Component
             user={user}
+            handleRecipes={handleRecipes}
+            getFavRecipes={getFavRecipes}
+            addToFavorites={addToFavorites}
             localMarkets={localMarkets}
             recipes={recipes}
+            favRecipes={favRecipes}
             ingredients={ingredients}
             userLocation={userLocation}
-            marketCoordinates={marketCoordinates}
             sessionZipcode={sessionZipcode}
             {...routeProps}
           />
