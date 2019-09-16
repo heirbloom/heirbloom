f6cb9a3f3cb42dd955dc1c7c083c2243199398d9
raphaelkhan8 <raphaelkhan8@gmail.com>
2019-09-15 22:59:32 -0500

    changed baseUrl to deployed url

--- react-client/src/constants.js
+++ react-client/src/constants.js
@@ -3,4 +3,4 @@
 const PORT = process.env.PORT || 3000
 console.log('PORT', PORT);
 
-export const baseUrl = `http://localhost:${PORT}`;
+export const baseUrl = `http://ec2-52-14-192-5.us-east-2.compute.amazonaws.com:${PORT}`;
