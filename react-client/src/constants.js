9a6c9e8821bf52fe7a7405ec618224464697a0a2
raphaelkhan8 <40365951+raphaelkhan8@users.noreply.github.com>
2019-11-02 15:07:55 -0500

    changed baseUrl variable to deployed url

--- react-client/src/constants.js
+++ react-client/src/constants.js
@@ -5,4 +5,6 @@ console.log('PORT', PORT);
 
 // change this to `http://localhost:${PORT}` when dev-ing
 // current url is the deployed one; api calls are using this variable
-export const baseUrl = `http://ec2-52-14-192-5.us-east-2.compute.amazonaws.com:${PORT}`;
+// export const baseUrl = 'http://localhost:3000';
+export const baseUrl = `https://heirbloom.herokuapp.com:${PORT}`;
+// export const baseUrl = `http://ec2-52-14-192-5.us-east-2.compute.amazonaws.com:${PORT}`;
